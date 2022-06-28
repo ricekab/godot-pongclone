0d4d324a4ee19ddb681a225e4f4e83f1d0160d5f
Kevin Chi Yan Tang <contact@kevinchiyantang.com>
2022-06-28 20:55:58 +0200

    Added bounce direction relative to paddle

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -16,5 +16,18 @@ func _ready():
 func _physics_process(delta: float):
 	var collisionInfo = move_and_collide(vel * delta)
 	if collisionInfo:
-		vel = vel.bounce(collisionInfo.normal)
-		print(collisionInfo)
+		if collisionInfo.collider.name == "paddle_p1" or \
+		   collisionInfo.collider.name == "paddle_p2":
+			var towards = self.position
+			var from = collisionInfo.collider.position
+			var direction_vector: Vector2 =  towards - from
+			direction_vector = direction_vector.normalized()
+			print(vel)
+			print(direction_vector)
+			#direction_vector = direction_vector * vel.length()
+			#vel = vel.move_toward(direction_vector, delta)
+			vel = direction_vector * vel.length()
+			print(vel)
+		else:
+			vel = vel.bounce(collisionInfo.normal)
+
