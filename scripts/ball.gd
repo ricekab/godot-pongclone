313f38857c73de20426e7ee97580855de3dc6492
Kevin Chi Yan Tang <contact@kevinchiyantang.com>
2022-06-29 19:20:16 +0200

    Clamped ball velocity to a max speed (constant) Removed print statements.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -8,6 +8,8 @@ extends KinematicBody2D
 export var vel = Vector2(1, 1)
 export var bounce_speed_scale = 1.10  # Vel increase on each bounce.
 
+const MAX_SPEED = 450.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,18 +19,13 @@ func _physics_process(delta: float):
 	var collisionInfo = move_and_collide(vel * delta)
 	if collisionInfo:
 		if collisionInfo.collider.name == "paddle_p1" or \
-		   collisionInfo.collider.name == "paddle_p2":
+			collisionInfo.collider.name == "paddle_p2":
 			var towards = self.position
 			var from = collisionInfo.collider.position
 			var direction_vector: Vector2 =  towards - from
 			direction_vector = direction_vector.normalized()
-			print(vel)
-			print(vel.length())
-			print(direction_vector)
-			#direction_vector = direction_vector * vel.length()
-			#vel = vel.move_toward(direction_vector, delta)
 			vel = direction_vector * vel.length()
-			print(vel)
 		else:
 			vel = vel.bounce(collisionInfo.normal)
-		vel *= bounce_speed_scale	
+		vel *= bounce_speed_scale
+		vel = vel.clamped(MAX_SPEED)
