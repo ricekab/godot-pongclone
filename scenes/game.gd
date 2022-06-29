fbfdd5ffc33d0f8c798d309cc080849936490050
Kevin Chi Yan Tang <contact@kevinchiyantang.com>
2022-06-29 19:21:07 +0200

    Initial ball velocity lowered, now goes straight so we don't have to move it to bounce (for testing). Added score tracking and signaling (UI not done yet)

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,6 +3,13 @@ extends Node2D
 var Paddle = preload("res://entities/paddle.tscn")
 var BallScript = preload("res://scripts/ball.gd")
 
+var RNG = RandomNumberGenerator.new()
+
+signal score_changed(left_score, right_score)
+
+var score_left = 0
+var score_right = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,16 +21,23 @@ func _spawn_new_ball():
 	var new_ball = Paddle.instance()
 	new_ball.set_script(BallScript)
 	new_ball.position = Vector2(400, 300)
-	new_ball.vel = Vector2(50, 50)
+	var rand_x = RNG.randf_range(0.5, 1.0)
+	var rand_y = RNG.randf_range(-1.0, 1.0)
+	var random_vel_normalized = Vector2(rand_x, rand_y).normalized()
+	new_ball.vel = random_vel_normalized * 100.0  # Initial speed
 	new_ball.set_collision_mask(3)  # layer 1 + 2
 	add_child(new_ball)
 
 
 func _on_left_score_area_body_entered(body):
-	print("RIGHT SIDE SCORED")
-	print(body)
+	score_right += 1
+	emit_signal("score_changed", score_left, score_right)
+	body.queue_free()
+	_spawn_new_ball()
 
 
 func _on_right_score_area_body_entered(body: PhysicsBody2D):
-	print("LEFT SIDE SCORED")
+	score_left += 1
+	emit_signal("score_changed", score_left, score_right)
 	body.queue_free()
+	_spawn_new_ball()
